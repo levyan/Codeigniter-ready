b954ed3988fb45585b216a053d44af1fed548685
37498717207@yandex.com <37498717207@yandex.com>
2016-02-28 14:14:50 +0400

    Codeigniter ready files

---
+++ application/config/core.php
@@ -0,0 +1,22 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+/**
+ * Core Config File
+ */
+
+// Site Details
+$config['site_version']          = "3.2.3";     // this is CI3 Fire Starter version - set it to 1.0.0 for your new project
+$config['root_folder']           = "";    // set to whatever your webroot is (htdocs, public_html, etc.) - MAKE SURE you rename the /htdocs folder locally
+$config['public_theme']          = "Default";   // folder containing your public theme
+$config['admin_theme']           = "Admin";     // folder containing your admin theme
+
+// Pagination
+$config['num_links']             = 8;
+$config['full_tag_open']         = "<div class=\"pagination\">";
+$config['full_tag_close']        = "</div>";
+
+// Miscellaneous
+$config['profiler']              = FALSE;
+$config['error_delimeter_left']  = "";
+$config['error_delimeter_right'] = "<br />";
